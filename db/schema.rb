2505fb80bfb390151e67b1156a22d0d0b2599400
andresochoa91 <andres_ochoa91@hotmail.com>
2020-09-30 23:08:56 -0700

    some updates in profiles

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20201001005523) do
+
   create_table "forums", force: :cascade do |t|
     t.string   "name"
     t.boolean  "public"
@@ -23,6 +24,35 @@ ActiveRecord::Schema.define(version: 20201001005523) do
     t.integer "forum_id", null: false
     t.index ["forum_id", "user_id"], name: "index_forums_users_on_forum_id_and_user_id"
     t.index ["user_id", "forum_id"], name: "index_forums_users_on_user_id_and_forum_id"
+  end
+
+  create_table "magazines", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "profiles", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "twitter"
+    t.string   "github"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
+
+  create_table "subscribers", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "subscriptions", force: :cascade do |t|
+    t.integer  "months"
+    t.integer  "subscriber_id"
+    t.integer  "magazine_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
   create_table "users", force: :cascade do |t|
